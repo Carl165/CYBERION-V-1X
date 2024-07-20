43f288cbd5c2a00eb93d6719cbbaa3204471e8cf
carl165 <167853149+Carl165@users.noreply.github.com>
2024-07-20 21:12:54 +0300

    Create Anticlone.js

---
+++ Anticlone.js
@@ -0,0 +1,4 @@
+>// fuck you...Don't 🍀 clone my repo]
+ " I will rape you and cum into your 👄 mouth
+   [`you homeless bitch filthy asshole young slut`]
+<h1 align="center"Clone at your own 🥘 risk...it will destroy your github accounts 
