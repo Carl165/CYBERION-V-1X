8f188e99ae059e9b6d2a4f1506b53c381d178bc5
carl165 <167853149+Carl165@users.noreply.github.com>
2024-07-22 09:33:06 +0300

    test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "repo", reaction: "🦁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗪𝗛𝗔𝗧"𝗦 𝗨𝗣 FÅM 𝗧𝗛𝗜𝗦 𝗜𝗦 CARL-TECH  \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *CARL-SHARK MD*𝗥𝗘𝗣𝗢.*";
+    let z = 'SSUP MAH KILLY🫠𝗧𝗛𝗜𝗦 𝗜𝗦 CARL-TECH  \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *CARL-SHARK MD*𝗥𝗘𝗣𝗢.*";
     let d = ' https://github.com/CARL165/CarlTech/tree/main';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/164dd0daed27a5330f912.jpg';
